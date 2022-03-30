2f8a259a70f5f643d6bcb97db2f9ac3b898df675
Brian Howard <bhoward@depauw.edu>
2022-03-29 20:58:17 -0400

    Updated UI javadoc

--- src/edu/depauw/csc232/solitaire/klondike/UI.java
+++ src/edu/depauw/csc232/solitaire/klondike/UI.java
@@ -4,6 +4,11 @@ import edu.depauw.csc232.solitaire.ui.Pile;
 import edu.depauw.csc232.solitaire.ui.PileStrategy;
 import edu.depauw.csc232.solitaire.ui.Table;
 
+/**
+ * Contains the user interface elements for a game of Klondike solitaire.
+ * 
+ * @author bhoward
+ */
 public class UI
 {
    public Pile waste;
@@ -11,11 +16,15 @@ public class UI
    public Pile[] tableau;
    public Pile[] foundation;
 
+   /**
+    * Construct the empty piles to be placed on the game table, and assign their
+    * interaction strategies.
+    */
    public UI()
    {
       PileStrategy wasteStrategy = new WasteStrategy();
       waste = Pile.makeSquared(wasteStrategy);
-      
+
       PileStrategy stockStrategy = new StockStrategy(this);
       stock = Pile.makeSquared(stockStrategy);
 
@@ -32,37 +41,45 @@ public class UI
       }
    }
 
+   /**
+    * Deal a deck of cards into the correct initial piles.
+    */
    public void dealGame()
    {
       stock.addDeck();
       stock.shuffle();
-      
+
       for (int i = 0; i < tableau.length; i++) {
          for (int j = i; j < tableau.length; j++) {
             tableau[j].add(stock.deal());
          }
-   
+
          // Flip the top cards in the tableau
          tableau[i].flipTop();
       }
    }
-   
+
+   /**
+    * Place the UI elements on the given Table.
+    * 
+    * @param table
+    */
    public void layoutUI(Table table)
    {
       waste.setX(110);
       waste.setY(10);
       table.addItem(waste);
-   
+
       stock.setX(10);
       stock.setY(10);
       table.addItem(stock);
-   
+
       for (int i = 0; i < tableau.length; i++) {
          tableau[i].setX(10 + 100 * i);
          tableau[i].setY(160);
          table.addItem(tableau[i]);
       }
-   
+
       for (int i = 0; i < foundation.length; i++) {
          foundation[i].setX(310 + 100 * i);
          foundation[i].setY(10);
